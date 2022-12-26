ab142cbef706508cfdf3e1ef385adbc9d46df628
Guil Sa <guilsa001@gmail.com>
2022-12-25 23:09:58 -0800

    Add basic Active Job

--- backend/app/controllers/api/v1/articles_controller.rb
+++ backend/app/controllers/api/v1/articles_controller.rb
@@ -1,3 +1,11 @@
+# Curl test:
+# curl \
+#   -H "Accept: application/json" \
+#   -H "Content-type: application/json" \
+#   -X POST \
+#   -d ' {"title":"Judge rules against Kari Lake in bid to overturn Arizona election results", "description": "The defeated GOP candidate for Arizona governor claimed that illegal voting and printer malfunctions had cost her the November election", "url": "https://www.washingtonpost.com/politics/2022/12/24/kari-lake-election-lawsuit/"}' \
+#   http://127.0.0.1:3000/api/v1/articles/
+
 module Api
   module V1
     class ArticlesController < ApplicationController
--- backend/app/jobs/scrape_memeorandum_job.rb
+++ backend/app/jobs/scrape_memeorandum_job.rb
@@ -1,7 +1,11 @@
+# ScrapeMemeorandumJob.perform_later(url: 'https://www.memeorandum.com/')
+
+# todo: have external cron job call api endpoint which invokes this
 class ScrapeMemeorandumJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
     # Do something later
+    puts "Scraping Memeorandum"
   end
 end
